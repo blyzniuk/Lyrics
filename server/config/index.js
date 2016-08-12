2b7790113dc147c4ece29062520b1a1dd040fccb
Oleksandr Blyzniuk <Oleksandr_Blyzniuk@epam.com>
2016-08-13 02:46:31 +0300

    Configured for heroku

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,5 @@
 const mongoUrl = 'mongodb://admin:admin@ds029565.mlab.com:29565/lyrics';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 module.exports.mongoUrl = mongoUrl;
 module.exports.port = port;
