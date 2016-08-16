406fd5941e572314bc8c1bd11f2880d9085b888b
Oleksandr Blyzniuk <Oleksandr_Blyzniuk@epam.com>
2016-08-16 23:51:37 +0300

    added getting few songs at once

--- api/songs/songs.controller.js
+++ api/songs/songs.controller.js
@@ -2,15 +2,16 @@
 
 const Artist = require('./artist.model');
 const mongoose = require('mongoose');
+const q = require('q');
+const util = require('util')
 
 class SongsController {
     getSong({ body: { artist, title } }, res) {
-        const collectionName = getCollectionName(artist);
-        const collection = mongoose.model('Artist', Artist, collectionName)
-
-        return collection.findOne({ artist })
+        return getArtist(artist)
             .then((artistObj) => {
-                const {original, ru} = artistObj.songs.find(({ original }) => original.title === title);
+                const { original, ru } = artistObj.songs.find(
+                    ({ original }) => original.title.toLowerCase() === title.toLowerCase()
+                );
                 res.status("200").json({ artist, original, ru, title });
             })
             .catch((err) => {
@@ -18,10 +19,42 @@ class SongsController {
             });
     }
 
-    getSongs(req, res) {
-        console.log('get songs');
-        return res.status(200).json({ "key": "value" });
+    getSongs({ body: { artists } }, res) {
+        const artistsSongsPromises = artists.map(({ artist, songs }) => getArtistRequestPromise(artist.toLowerCase(), songs));
+
+        return q.all(artistsSongsPromises)
+            .then((result) => res.status(200).json(result))
+            .catch((err) => {
+                res.status("404").json(err);
+            });
     }
+}
+
+function getArtistRequestPromise(artist, songs) {
+    return getArtist(artist)
+        .then((artistObj) => ({
+            artist,
+            songs: getArtistSongs(artistObj, songs)
+        }));
+}
+
+function getArtistSongs({ songs }, neededSongs) {
+    return songs.reduce(
+        (result, song) => {
+            if (isSongRequested(neededSongs, song)) {
+                result.push(song);
+            }
+            return result;
+        }, []
+    );
+}
+
+function isSongRequested(neededSongs, song) {
+    return neededSongs.some((title) => song.original.title.toLowerCase() === title.toLowerCase())
+}
+
+function getArtist(artist) {
+    return mongoose.model('Artist', Artist, getCollectionName(artist)).findOne({ artist });
 }
 
 function getCollectionName(artistName) {
